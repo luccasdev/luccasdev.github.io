f5576ce6e2941bb15299618ff382c6b706574ecb
luccasdev <lucasrti@hotmail.com>
2020-08-24 18:01:29 -0300

    Projects component

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -9,6 +9,8 @@ export class ProjectsComponent implements OnInit {
 
   readonly GIT_HUB_URL = 'https://github.com/luccasdev';
 
+  projects: Project[];
+
   constructor() { }
 
   ngOnInit(): void {
@@ -17,4 +19,11 @@ export class ProjectsComponent implements OnInit {
   openGitHubPage() {
     window.open(this.GIT_HUB_URL, '_blank');
   }
+}
+
+export class Project {
+  title: string;
+  description: string;
+  countStars: number;
+  countForks: number;
 }
